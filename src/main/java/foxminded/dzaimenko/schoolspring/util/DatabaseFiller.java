2f3ef87a2914361563425c925ae4eb033da30848
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2024-02-21 20:16:57 +0200

    Fixed DatabaseFiller logic (now use only real ID from base)

--- src/main/java/foxminded/dzaimenko/schoolspring/util/DatabaseFiller.java
+++ src/main/java/foxminded/dzaimenko/schoolspring/util/DatabaseFiller.java
@@ -1,10 +1,15 @@
 package foxminded.dzaimenko.schoolspring.util;
 
+import foxminded.dzaimenko.schoolspring.model.Course;
+import foxminded.dzaimenko.schoolspring.model.Group;
+import foxminded.dzaimenko.schoolspring.model.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -17,6 +22,15 @@ public class DatabaseFiller {
     private final String SQL_INSERT_STUDENTS = "INSERT INTO students (group_id, first_name, last_name) VALUES (?, ?, ?)";
     private final String SQL_INSERT_COURSES = "INSERT INTO courses (course_name, course_description) VALUES (?,?)";
     private final String SQL_INSERT_STUDENT_COURSES = "INSERT INTO student_courses (student_id, course_id) VALUES (?,?)";
+
+    private final String SQL_GET_GROUP_ID = "SELECT group_id FROM groups WHERE group_name = ?";
+    private final String SQL_GET_COURSE_ID = "SELECT course_id FROM courses WHERE course_name = ?";
+    private final String SQL_GET_STUDENT_ID = "SELECT student_id FROM students WHERE first_name = ? AND last_name = ?";
+
+    public final List<Group> groups = new ArrayList<>();
+    public final List<Course> courses = new ArrayList<>();
+
+    public final List<Student> students = new ArrayList<>();
 
     public DatabaseFiller(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -35,32 +49,75 @@ public class DatabaseFiller {
 
         for (int i = 0; i < 10; i++) {
             String groupName = SchoolData.groupsNames[i];
+            Group group = new Group();
+            group.setName(groupName);
+
 
             jdbcTemplate.update(SQL_INSERT_GROUPS, groupName);
+
+            Integer id = jdbcTemplate.queryForObject(SQL_GET_GROUP_ID, Integer.class, groupName);
+            group.setId(id);
+
+            groups.add(group);
         }
     }
 
-    private void studentsTableFill() {
+    private Set<String> generateUniqueStudentsSet() {
+        Set<String> uniqueStudents = new HashSet<>();
 
-        for (int i = 1; i <= 200; i++) {
-
-            int groupId = random.nextInt(10) + 1;
+        while (uniqueStudents.size() < 200) {
             String firstName = SchoolData.firstNamesArray[random.nextInt(20)];
             String lastName = SchoolData.lastNamesArray[random.nextInt(20)];
 
+            String uniqueKey = firstName + " " + lastName;
+
+            uniqueStudents.add(uniqueKey);
+        }
+
+        return uniqueStudents;
+    }
+
+    private void studentsTableFill() {
+        Set<String> uniqueStudents = generateUniqueStudentsSet();
+
+        for (String uniqueKey : uniqueStudents) {
+            Integer groupId = groups.get(random.nextInt(10)).getId();
+            String[] nameParts = uniqueKey.split(" ");
+            String firstName = nameParts[0];
+            String lastName = nameParts[1];
+
+            Student student = new Student();
+            student.setStudentId(groupId);
+            student.setFirstName(firstName);
+            student.setLastName(lastName);
+
             jdbcTemplate.update(SQL_INSERT_STUDENTS, groupId, firstName, lastName);
+
+            Integer id = jdbcTemplate.queryForObject(SQL_GET_STUDENT_ID, Integer.class, firstName, lastName);
+            student.setGroupId(id);
+
+            students.add(student);
         }
     }
 
     private void coursesTableFill() {
 
         for (int i = 0; i < 10; i++) {
-
             String courseName = SchoolData.coursesNames[i];
             String courseDescription = SchoolData.coursesDescriptions[i];
 
+            Course course = new Course();
+            course.setName(courseName);
+            course.setDescription(courseDescription);
+
             jdbcTemplate.update(SQL_INSERT_COURSES, courseName, courseDescription);
+
+            Integer id = jdbcTemplate.queryForObject(SQL_GET_COURSE_ID, Integer.class, courseName);
+            course.setId(id);
+
+            courses.add(course);
         }
+
     }
 
     private void studentsCoursesTableFill() {
@@ -71,7 +128,7 @@ public class DatabaseFiller {
             int numberOfCourses = random.nextInt(3) + 1;
 
             while (assignedCourses.size() < numberOfCourses) {
-                int courseId = random.nextInt(10) + 1;
+                int courseId = courses.get(random.nextInt(10)).getId();
 
                 if (assignedCourses.add(courseId)) {
                     jdbcTemplate.update(SQL_INSERT_STUDENT_COURSES, studentId, courseId);
